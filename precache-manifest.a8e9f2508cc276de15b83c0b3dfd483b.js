284f31877a5e05fedc777bb0435e70c75247fa8c
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-01-07 18:52:21 +0000

    Updates

---
+++ precache-manifest.a8e9f2508cc276de15b83c0b3dfd483b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cf737ec8b7e6204f92086851792570",
+    "url": "/kumis-kitchen/index.html"
+  },
+  {
+    "revision": "9bfe14ec9999b9b998ea",
+    "url": "/kumis-kitchen/static/css/main.26ec6138.chunk.css"
+  },
+  {
+    "revision": "e5f302a468267b333f4c",
+    "url": "/kumis-kitchen/static/js/2.c781f276.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/kumis-kitchen/static/js/2.c781f276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bfe14ec9999b9b998ea",
+    "url": "/kumis-kitchen/static/js/main.e15bb6cf.chunk.js"
+  },
+  {
+    "revision": "1b92f3dab5cdb5e174a0",
+    "url": "/kumis-kitchen/static/js/runtime-main.a7d0090c.js"
+  },
+  {
+    "revision": "d698f2a9dd86c2ddf128da375ff513b5",
+    "url": "/kumis-kitchen/static/media/happy_little_chef.d698f2a9.png"
+  },
+  {
+    "revision": "257e88d91580f167d1feafb834df6a00",
+    "url": "/kumis-kitchen/static/media/pexels-lukas-952478.257e88d9.jpg"
+  }
+]);
